f57711c5b24ee51e2b75ed671dcc7f2443e11091
Iordancho <127532874+Iordancho@users.noreply.github.com>
2023-11-11 21:44:45 +0200

    Delete Entity Framework Entiry Relations directory

--- Entity Framework Entiry Relations/P01_StudentSystem/P01_StudentSystem.Data.Models/Homework.cs
+++
@@ -1,7 +0,0 @@
-namespace P01_StudentSystem.P01_StudentSystem.Data.Models;
-
-public class Homework
-{
-    public int HomeworkId { get; set; }
-    
-}
--- Entity Framework Entiry Relations/P01_StudentSystem/P01_StudentSystem.Data.Models/ResourceType.cs
+++
@@ -1,9 +0,0 @@
-namespace P01_StudentSystem.P01_StudentSystem.Data.Models;
-
-public enum ResourceType
-{
-    Video,
-    Presentation,
-    Document,
-    Other
-}
